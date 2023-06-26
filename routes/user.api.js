36d7e36c53930058d2faaa14b083d45d8086b155
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-27 02:00:21 +0700

    Update user.api.js

--- routes/user.api.js
+++ routes/user.api.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const { register } = require("../controllers/user.controller");
 const router = express.Router();
-const { body } = require("express-validator");
-
+const userController = require("../controllers/user.controller");
+const { body, param } = require("express-validator");
 const validators = require("../middlewares/validators");
+const authentication = require("../middlewares/authentication");
 
 /**
  * @route POST /users
- * @description Register new user
- * @body {email, password}
+ * @description  Register new user
+ * @body {name, email, password}
  * @access Public
  */
 router.post(
@@ -19,34 +19,56 @@ router.post(
       .exists()
       .isEmail()
       .normalizeEmail({ gmail_remove_dots: false }),
-    body("password", "Invaliad password").exists().notEmpty(),
+    body("password", "Invalid password").exists().notEmpty(),
   ]),
-  register
+  userController.register
 );
 
 /**
- * @route GET /users?page=1&limit=10
- * @description Get users with pagination
+ * @route GET/users?page=1&limit=10
+ * @description  Get users with pagination
  * @access Login required
  */
 
+router.get("/", authentication.loginRequired, userController.getUsers);
+
 /**
- * @route GET /users/me
- * @description Get curent user info
+ * @route GET/users/me
+ * @description Get current user info
  * @access Login required
  */
 
+router.get("/me", authentication.loginRequired, userController.getCurrentUser);
+
 /**
- * @route GET /users/:id
- * @description Get a user profile
+ * @route GET/users/:id
+ * @description Update user profile
+ * @body { name, avatarUrl, coverUrl, aboutMe, city, county, company, jobTitle, facebookLink, instagramLink, linkedinLink, twitterLink }
  * @access Login required
  */
+
+router.get(
+  "/:id",
+  authentication.loginRequired,
+  validators.validate([
+    param("id").exists().isString().custom(validators.checkObjectId),
+  ]),
+  userController.getSingleUser
+);
 
 /**
  * @route PUT /users/:id
- * @description Update user profile
- * @body {name, avatarUrl, coverUrl, aboutMe, city, country, company, jobTitle, facebookLink, instagramLink, linkedinLink, twitterLink}
+ * @description update user profile
+ * @body {name, avatarUrl, coverUrl, aboutMe, city, country, company, jobTitle, facebookLink, instagramLink, linkedLink, twitterLink}
  * @access Login required
  */
 
+router.put(
+  "/:id",
+  authentication.loginRequired,
+  validators.validate([
+    param("id").exists().isString().custom(validators.checkObjectId),
+  ]),
+  userController.updateProfile
+);
 module.exports = router;
