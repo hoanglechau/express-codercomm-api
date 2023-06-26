0fdb6f9827d8f9fa3a1f2d3f4bb92d9af1f31454
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-27 02:00:13 +0700

    Update friend.api.js

--- routes/friend.api.js
+++ routes/friend.api.js
@@ -2,44 +2,45 @@ const express = require("express");
 const router = express.Router();
 
 /**
- * @route POST /friends/requests
+ * @route POST/friends/request
  * @description Send a friend request
- * @body {to: user ID}
+ * @body { to: User ID }
  * @access Login required
  */
 
 /**
- * @route GET /friends/requests/incomming
+ * @route GET/friends/requests/incoming
  * @description Get the list of received pending requests
  * @access Login required
  */
 
 /**
- * @route GET /friends/requests/outgoing
+ * @route GET/friends/requests/outgoing
  * @description Get the list of sent pending requests
  * @access Login required
  */
 
 /**
- * @route GET /friends
+ * @route GET/friends
  * @description Get the list of friends
  * @access Login required
  */
 
 /**
- * @route PUT /friends/requests/:userId
- * @description Acceipt/Reject a received pending request
+ * @route PUT/friends/request/:userId
+ * @description Accept/Reject a received pending requests
+ * @body { status: 'accepted' or 'declined' }
  * @access Login required
  */
 
 /**
- * @route DELETE /friends/requests/:userId
+ * @route DELETE/friends/request/:userId
  * @description Cancel a friend request
  * @access Login required
  */
 
 /**
- * @route DELETE /friends/:userId
+ * @route DELETE/friends/:userId
  * @description Remove a friend
  * @access Login required
  */
