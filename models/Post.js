e493ee63c67a9008c7ff1d0055ff196469e4c5d6
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-27 02:00:01 +0700

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -3,20 +3,46 @@ const Schema = mongoose.Schema;
 
 const postSchema = Schema(
   {
-    content: { type: String, require: true },
-    image: { type: String, default: "" },
-    author: { type: Schema.Types.ObjectId, require: true, ref: "User" },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    author: {
+      type: String,
+      required: true,
+      ref: "User",
+    },
 
-    isDelete: { type: Boolean, default: false, select: false },
-    commentCount: { type: Number, default: 0 },
-    reaction: {
-      like: { type: Number, default: 0 },
-      dislike: { type: Number, default: 0 },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+      select: false,
+    },
+
+    commentCount: {
+      type: Number,
+      default: 0,
+    },
+
+    reactions: {
+      like: {
+        type: Number,
+        default: 0,
+      },
+      dislike: {
+        type: Number,
+        default: 0,
+      },
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 const Post = mongoose.model("Post", postSchema);
-
 module.exports = Post;
