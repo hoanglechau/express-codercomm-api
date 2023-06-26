1419ebc23dd7e61e7ca55cee4a780cd50ac41837
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-27 02:00:03 +0700

    Update Reaction.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -3,18 +3,32 @@ const Schema = mongoose.Schema;
 
 const reactionSchema = Schema(
   {
-    author: { type: Schema.Types.ObjectId, require: true, ref: "User" },
-    targetType: { type: String, required: true, enum: ["Post", "Comment"] },
-    targetId: {
+    author: {
+      type: Schema.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    targetType: {
       type: String,
       required: true,
-      refPath: "targetType", //reference to Post or Comment depend on value of targetType
+      enum: ["Post", "Comment"],
     },
-    emoji: { type: String, required: true, enum: ["like", "dislike"] },
+    targetId: {
+      type: Schema.ObjectId,
+      required: true,
+      refPath: "targetType",
+    },
+
+    emoji: {
+      type: String,
+      required: true,
+      enum: ["like", "dislike"],
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 const Reaction = mongoose.model("Reaction", reactionSchema);
-
 module.exports = Reaction;
