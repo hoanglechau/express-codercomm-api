b31a92a10176110f03aefdf96de772d5cbe153c3
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-27 02:00:17 +0700

    Update post.api.js

--- routes/post.api.js
+++ routes/post.api.js
@@ -1,36 +1,95 @@
 const express = require("express");
+const authentication = require("../middlewares/authentication");
+const validators = require("../middlewares/validators");
 const router = express.Router();
+const { body, param } = require("express-validator");
+const postController = require("../controllers/post.controller");
 
 /**
- * @route GET /posts/user/:userId?page=1&limit=10
+ * @route GET/posts/user/:userId?page=1&limit=10
  * @description Get all posts an user can see with pagination
  * @access Login required
  */
 
+router.get(
+  "/user/:userId",
+
+  validators.validate([
+    param("userId").exists().isString().custom(validators.checkObjectId),
+  ]),
+  postController.getPosts
+);
 /**
- * @route POST /posts
+ * @route POST/posts
  * @description Create a new post
- * @body {content, image}
+ * @body { content, image }
  * @access Login required
  */
 
+router.post(
+  "/",
+  authentication.loginRequired,
+  validators.validate([body("content", "Missing content").exists().notEmpty()]),
+  postController.createNewPost
+);
+
 /**
- * @route PUT /posts
+ * @route PUT/posts/:id
  * @description Update a post
- * @body {content, image}
+ * @body { content, image }
  * @access Login required
  */
 
+router.put(
+  "/:id",
+  authentication.loginRequired,
+  validators.validate([
+    param("id").exists().isString().custom(validators.checkObjectId),
+  ]),
+  postController.updateSinglePost
+);
+
 /**
- * @route DELETE /posts/:id
+ * @route DELETE/posts/:id
  * @description Delete a post
  * @access Login required
  */
 
+router.delete(
+  "/:id",
+  authentication.loginRequired,
+  validators.validate([
+    param("id").exists().isString().custom(validators.checkObjectId),
+  ]),
+  postController.deleteSinglePost
+);
+
 /**
- * @route GET /posts/:id/comments
- * @description Get all comments of a post
+ * @route GET/posts/:id
+ * @description Get a single post
  * @access Login required
  */
 
+router.get(
+  "/:id",
+  authentication.loginRequired,
+  validators.validate([
+    param("id").exists().isString().custom(validators.checkObjectId),
+  ]),
+  postController.getSinglePost
+);
+/**
+ * @route GET/posts/:id/comments
+ * @description Get comments of a post.
+ * @access Login required
+ */
+
+router.get(
+  "/:id/comments",
+  authentication.loginRequired,
+  validators.validate([
+    param("id").exists().isString().custom(validators.checkObjectId),
+  ]),
+  postController.getCommentsOfPost
+);
 module.exports = router;
