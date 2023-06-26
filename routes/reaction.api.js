8baf62c0d7008f54856ec1a2d28545d52c28ee65
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-27 02:00:19 +0700

    Update reaction.api.js

--- routes/reaction.api.js
+++ routes/reaction.api.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const { body } = require("express-validator");
+const reactionController = require("../controllers/reaction.controller");
+const authentication = require("../middlewares/authentication");
+const validators = require("../middlewares/validators");
 const router = express.Router();
 
 /**
- * @route POST /reactions
+ * @route POST/reactions
  * @description Save a reaction to post or comment
- * @body {targetType: 'Post' or 'Comment', targetId, emoji: 'like' or 'dislike'}
+ * @body { targetType: 'Post' or 'Comment', targetId, emoji: 'like' or 'dislike' }
  * @access Login required
  */
+
+router.post(
+  "/",
+  authentication.loginRequired,
+  validators.validate([
+    body("targetType", "Invalid targetType").exists().isIn(["Post", "Comment"]),
+    body("targetId", "Invalid targetId")
+      .exists()
+      .custom(validators.checkObjectId),
+    body("emoji", "Invalid emoji").exists().isIn(["like", "dislike"]),
+  ]),
+  reactionController.saveReaction
+);
 
 module.exports = router;
