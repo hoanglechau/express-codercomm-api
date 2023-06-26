fed0e2349b18966b006c6c65c1688aeaf55b8bbc
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-27 02:00:05 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,42 +5,108 @@ const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 const userSchema = Schema(
   {
-    name: { type: String, require: true },
-    email: { type: String, require: true, unique: true },
-    password: { type: String, require: true, select: false },
-    avatarUrl: { type: String, require: false, default: "" },
-    coverUrl: { type: String, require: false, default: "" },
-    aboutMe: { type: String, require: false, default: "" },
-    city: { type: String, require: false, default: "" },
-    country: { type: String, require: false, default: "" },
-    company: { type: String, require: false, default: "" },
-    jobTitle: { type: String, require: false, default: "" },
-    facebookLink: { type: String, require: false, default: "" },
-    instagramLink: { type: String, require: false, default: "" },
-    linkedinLink: { type: String, require: false, default: "" },
-    twitterLink: { type: String, require: false, default: "" },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatarUrl: {
+      type: String,
+      require: false,
+      default: "",
+    },
+    coverUrl: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    aboutMe: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    city: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    country: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    company: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    jobTitle: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    facebookLink: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    instagramLink: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    linkedinLink: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    twitterLink: {
+      type: String,
+      required: false,
+      default: "",
+    },
 
-    isDelete: { type: Boolean, default: false, select: false },
-    friendCount: { type: Number, default: 0 },
-    postCount: { type: Number, default: 0 },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+      select: false,
+    },
+
+    friendCount: {
+      type: Number,
+      default: 0,
+    },
+    postCount: {
+      type: Number,
+      default: 0,
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
+//
 userSchema.methods.toJSON = function () {
   const user = this._doc;
   delete user.password;
-  delete user.isDelete;
+  delete user.isDeleted;
   return user;
 };
 
 userSchema.methods.generateToken = async function () {
   const accessToken = await jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
-    expirersIn: "1d",
+    expiresIn: "1d",
   });
   return accessToken;
 };
-
 const User = mongoose.model("User", userSchema);
-
 module.exports = User;
