3d4269eca60c208e03d10b43f43f5c12fdda22bd
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-27 01:59:41 +0700

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,29 +1,28 @@
-const User = require("../models/User");
-const { sendResponse, AppError, catchAsync } = require("../helpers/utils");
+const { AppError, catchAsync, sendResponse } = require("../helpers/utils");
+const User = require("../model/User");
 const bcrypt = require("bcryptjs");
 const authController = {};
 
 authController.loginWithEmail = catchAsync(async (req, res, next) => {
   // Get data from request
-  let { email, password } = req.body;
-  // Validation
-  let user = await User.findOne({ email }, "+password");
-  if (!user) throw new AppError(400, "Invalid Credentials", "Login Error");
-  // Process
+  const { email, password } = req.body;
 
+  // Bussiness logic Validation
+  const user = await User.findOne({ email }, "+password");
+  if (!user) {
+    throw new AppError(400, "Invalid Credentials", "Login Error");
+  }
+  // Process
+  // mã hóa password
   const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) throw new AppError(400, "Wrong password", "Login Error");
+  if (!isMatch) {
+    throw new AppError(400, "Wrong password", "Login Error");
+  }
+
   const accessToken = await user.generateToken();
   // Response
 
-  sendResponse(
-    res,
-    200,
-    true,
-    { user, accessToken },
-    null,
-    "Create User Successfully"
-  );
+  sendResponse(res, 200, true, { user, accessToken }, null, "Login successful");
 });
 
 module.exports = authController;
