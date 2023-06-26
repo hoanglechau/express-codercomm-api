8e63f16694b59d2fc59559f90fe5143b866bb2d1
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-06-27 02:00:11 +0700

    Update comment.api.js

--- routes/comment.api.js
+++ routes/comment.api.js
@@ -1,30 +1,75 @@
 const express = require("express");
+const { body, param } = require("express-validator");
+const commentController = require("../controllers/comment.controller");
+const authentication = require("../middlewares/authentication");
+const validators = require("../middlewares/validators");
 const router = express.Router();
 
 /**
- * @route POST /comments
+ * @route POST/comments/
  * @description Create a new comment
- * @body {content, image}
+ * @body { content, postId }
  * @access Login required
  */
 
+router.post(
+  "/",
+  authentication.loginRequired,
+  validators.validate([
+    body("content", "Missing content").exists().notEmpty(),
+    body("postId", "Missing postId")
+      .exists()
+      .isString()
+      .custom(validators.checkObjectId),
+  ]),
+  commentController.createNewComment
+);
+
 /**
- * @route PUT /comments/:id
+ * @route PUT/comments/:id
  * @description Update a comment
- * @body {content, image}
  * @access Login required
  */
 
+router.put(
+  "/:id",
+  authentication.loginRequired,
+  validators.validate([
+    param("id").exists().isString().custom(validators.checkObjectId),
+    body("content", "Missing content").exists().notEmpty(),
+  ]),
+  commentController.updateSingleComment
+);
+
 /**
- * @route DELETE /comments/:id
+ * @route DELETE/comments/:id
  * @description Delete a comment
+ * @body { content, postId }
  * @access Login required
  */
 
+router.delete(
+  "/:id",
+  authentication.loginRequired,
+  validators.validate([
+    param("id").exists().isString().custom(validators.checkObjectId),
+  ]),
+  commentController.deleteSingleComment
+);
+
 /**
- * @route GET /comments/:id
- * @description Get details of a comment
+ * @route GET/comments/:id
+ * @description Get details of comment
  * @access Login required
  */
+
+router.get(
+  "/:id",
+  authentication.loginRequired,
+  validators.validate([
+    param("id").exists().isString().custom(validators.checkObjectId),
+  ]),
+  commentController.getSingleComment
+);
 
 module.exports = router;
